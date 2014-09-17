98db9341343493aafc9653fddbd29a7d9dc2c369
Jan Pipek <jan.pipek@gmail.com>
2014-09-17 12:10:40 +0200

    register subtree generators in file

--- boadata/trees/file.py
+++ boadata/trees/file.py
@@ -1,6 +1,15 @@
 from ..core import DataNode
 import os
 import mimetypes
+from collections import OrderedDict
+
+tree_generators = {}
+
+object_generators = {}
+
+def register_tree_generator(mime_type, func):
+    tree_generators[mime_type] = func
+
 
 class PathNode(DataNode):
     def __init__(self, path, parent=None):
@@ -15,8 +24,17 @@ class PathNode(DataNode):
     def mime_type(self):
         return mimetypes.guess_type(self.path)   
 
+
 class FileNode(PathNode):
-    pass
+    def has_subtree(self):
+        return self.mime_type[0] and self.mime_type[0] in tree_generators 
+
+    def subtree(self):
+        if not self.has_subtree():
+            return None
+        else:
+            return tree_generators[self.mime_type[0]](self.path)
+
 
 class DirectoryNode(PathNode):
     def load_children(self):
