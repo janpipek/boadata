73ae091d2025d5322de519e448cafecf1ede7b6b
Jan Pipek <jan.pipek@gmail.com>
2014-09-17 12:09:56 +0200

    Simplify conversions

--- boadata/core/data_object.py
+++ boadata/core/data_object.py
@@ -9,6 +9,15 @@ class DataObject(object):
 
         It is possible to override this property if conversion list is not static.
         '''
-        return tuple(attr.strip("_").partition("_")[2] for attr in dir(self) if attr.startswith("__as_") and attr.endswith("__"))
+        return tuple(attr.partition("_")[2] for attr in dir(self) if attr.startswith("as_"))
 
+    def to(self, conversion):
+        attr = "as_" + conversion
+        if not hasattr(self, attr):
+            raise "Conversion not supported."
+        return getattr(self, attr)()
+
+    @property
+    def data_object(self):
+        return self
 
