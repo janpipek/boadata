bc5a22545730fc714d1b742c9b2909414635b140
Jan Pipek <pipek@ipp.cas.cz>
2014-10-08 17:27:04 +0200

    Concept of backends in plot

--- boadata/gui/qt/views/xy_plot_view.py
+++ boadata/gui/qt/views/xy_plot_view.py
@@ -1,9 +1,37 @@
 from view import View, register_view
-import pyqtgraph as pg
 import numpy as np
+
+backends = []
+
+# Try import pyqtgraph backend
+try:
+    import pyqtgraph as pg
+
+    class PyQtGraphBackend:
+        @classmethod
+        def create_plot_widget(cls, x, y):
+            pw = pg.PlotWidget()
+            pw.plot(x, y)
+            return pw
+    backends.append(PyQtGraphBackend)
+except:
+    pass
+
+# Try import matplotlib backend
+try:
+    from matplotlib.figure import Figure
+    from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+
+except:
+    pass
+
 
 class XYPlotView(View):
     title = "XY Plot"
+
+    def __init__(self, data_object):
+        super(XYPlotView, self).__init__(data_object)
+        self.backend = backends[0]
 
     @classmethod
     def accepts(cls, data_object):
@@ -35,8 +63,6 @@ class XYPlotView(View):
                     raise Exception("Invalid 2D matrix to plot")
             else:
                 raise Exception("Invalid matrix to plot")
-        pw = pg.PlotWidget()
-        pw.plot(x, y)
-        return pw
+        return self.backend.create_plot_widget(x, y)
 
 register_view(XYPlotView)
