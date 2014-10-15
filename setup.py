ecb4784cf183087095b400d83f159e75a6d66562
Jan Pipek <jan.pipek@gmail.com>
2014-10-15 14:07:19 +0200

    PyPi package

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='boadata',
+    version='0.1',
+    packages=find_packages(),
+    license='MIT',
+    description='(B)rowser (O)f (A)rbitrary Data - a Python GUI browser of data.',
+    long_description=open('README.md').read(),
+    author='Jan Pipek',
+    author_email='jan.pipek@gmail.com',
+    url='https://github.com/janpipek/boadata',
+    install_requires = [ 'numpy', 'pandas' ],
+    entry_points = {
+        'console_scripts' : [
+            'boadata = boadata:run_app'
+        ]
+    }
+)
