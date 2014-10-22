fc8c7df1023b8b56308160f8767ae41217adbcb9
Jan Pipek <jan.pipek@gmail.com>
2014-10-22 17:31:55 +0200

    DataNode.changed signal

--- boadata/core/data_node.py
+++ boadata/core/data_node.py
@@ -19,6 +19,7 @@ class DataNode(object):
     # Signals
     child_added = blinker.Signal("child_added")
     child_removed = blinker.Signal("child_removed")
+    changed = blinker.Signal("changed")
 
     @property
     def icon(self):
@@ -88,14 +89,17 @@ class DataNode(object):
 
     def add_child(self, child, send_signal=True):
         child.parent = self
+        child.changed.connect(lambda _: self.changed.send(self), sender=child)
         self._children.append(child)
         if send_signal:
             self.child_added.send(self, child=child)
+            self.changed.send(self)
 
     def remove_child(self, child, send_signal=True):
         self._children.remove(child)
         if send_signal:
             self.child_removed.send(self, child=child)
+            self.changed.send(self)
 
     def load_children(self):
         pass
@@ -104,6 +108,7 @@ class DataNode(object):
         '''Forces children reloading.'''
         self._children = []
         self.children_loaded = False
+        self.changed.send(self)
 
     def dump(self, stream=sys.stdout, indent=u"  ", subtree=False, in_depth=0, children_only=False, data_object_info=False):
         if not children_only:
