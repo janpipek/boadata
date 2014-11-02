594d5a641ddaab1394e390fadc7c20ffb94fcc20
Jan Pipek <jan.pipek@gmail.com>
2014-11-02 14:28:29 +0100

    Move data types into own package

--- boadata/core/__init__.py
+++ boadata/core/__init__.py
@@ -1,7 +1,5 @@
 '''Classes for data abstraction used in boadata.'''
 
-from data_types import SelectableMixin, DictionaryNotifierMixin
 from data_properties import DataProperties
 from data_object import DataObject
 from data_node import DataNode, DataTree
-from selectable_item_list import SelectableItemList
---
+++ boadata/core/data_types/__init__.py
@@ -0,0 +1,2 @@
+'''This module contains a few advanced data types.'''
+from selectable_item_list import SelectableItemList
