7004abfb9ce97645c56eaa7caceebd6d94d86f19
Jan Pipek <jan.pipek@gmail.com>
2014-11-02 14:27:58 +0100

    Introduce tests (real beginning)

---
+++ tests/gui/__init__.py
@@ -0,0 +1,4 @@
+from PyQt4 import QtGui
+qapp = QtGui.QApplication([]) # Has to be created prior to testing
+
+import selectable_item_list_view_test
---
+++ tests/gui/selectable_item_list_view_test.py
@@ -0,0 +1,26 @@
+from boadata.gui.qt.selectable_item_list_view import SelectableItemListView
+from boadata.core.data_types import SelectableItemList
+from PyQt4 import QtGui
+
+import unittest
+
+class SelectableItemListViewTest(unittest.TestCase):
+    def test_create_from_empty_list(self):
+        item_list = SelectableItemList()
+        view = SelectableItemListView(item_list)
+        self.assertEqual(item_list, view.item_list)
+
+    def test_create_from_wrong_objects(self):
+        item_list = []
+        self.assertRaises(BaseException, SelectableItemListView, item_list)
+
+        item_list = {}
+        self.assertRaises(BaseException, SelectableItemListView, item_list)
+
+    def test_correct_item_creation_in_init(self):
+        item_list = SelectableItemList()
+        item_list["aaa"] = "bbb"
+
+        view = SelectableItemListView(item_list)
+        assert "aaa" in view.items
+        assert isinstance(view.items["aaa"], QtGui.QStandardItem)
