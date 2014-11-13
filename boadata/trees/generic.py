a92c84d57eae173303ecf7200bf3ee94f83c8f82
Jan Pipek <jan.pipek@gmail.com>
2014-11-14 00:59:23 +0100

    Generic tree to accept any node

---
+++ boadata/trees/generic.py
@@ -0,0 +1,16 @@
+from ..core.data_node import DataTree
+
+class GenericTree(DataTree):
+    '''A tree that accepts adding nodes of any type.'''
+    def __init__(self, parent=None):
+        super(GenericTree, self).__init__(parent)
+        self._title = "Generic tree"
+        self.children_loaded = True  # Empty by default
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, new_title):
+        self._title = new_title
