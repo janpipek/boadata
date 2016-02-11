1dfaf18c405395625ce15769d1aaaefc578b1de2
Jan Pipek <jan.pipek@gmail.com>
2016-02-11 08:47:43 +0100

    plot ... basics

---
+++ boadata/commands/boaplot.py
@@ -0,0 +1,23 @@
+from boadata.core import DataObject
+from boadata.gui.qt.views import PlotView
+import sys
+
+from boadata.gui import qt   # Force sip
+from PyQt4 import QtGui
+
+def run_app():
+    uri = sys.argv[1]
+    try:
+        do = DataObject.from_uri(uri)
+    except:
+        print("URI not understood.")
+        exit(-1)
+
+    app = QtGui.QApplication(sys.argv)
+
+    view = PlotView(data_object=do)
+    widget = view.create_widget()
+    widget.show()
+    widget.setWindowTitle(do.uri)
+
+    sys.exit(app.exec_())
---
+++ boadata/gui/qt/views/plot_view.py
@@ -0,0 +1,12 @@
+from .view import View
+
+
+@View.register_view
+class PlotView(View):
+    def accepts(cls, data_object):
+        if data_object.ndim == 2 and (data_object.shape[0] == 2 or data_object.shape[1] == 2):
+            return True
+        return False
+
+    def create_widget(self):
+
