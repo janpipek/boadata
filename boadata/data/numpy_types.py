d520d4e48620c07eaefdf2f10710bace278020a2
Jan Pipek <jan.pipek@gmail.com>
2016-02-15 17:23:28 +0100

    HDF5->pd.Series, CSV; ndarray->pd.Series

--- boadata/data/numpy_types.py
+++ boadata/data/numpy_types.py
@@ -1,9 +1,11 @@
 from boadata.core import DataObject
-from boadata.core.data_conversion import DataConversion, OdoConversion
+from boadata.core.data_conversion import DataConversion, OdoConversion, ConstructorConversion
 import numpy as np
 import pandas as pd
 
 
+@ConstructorConversion.enable_to("pandas_data_frame", condition=lambda x: x.ndim == 2)
+@ConstructorConversion.enable_to("pandas_series", condition=lambda x: x.ndim == 1)
 @DataObject.register_type
 class NumpyArray(DataObject):
     real_type = np.ndarray
@@ -23,9 +25,3 @@ class NumpyArray(DataObject):
         np.savetxt(uri, self.inner_data, delimiter=",")
         csv_type = DataObject.registered_types["csv"]
         return csv_type.from_uri(uri, source=self)
-
-    @DataConversion.register("numpy_array", "pandas_data_frame", condition=lambda x: x.ndim == 2)
-    def to_pandas_data_frame(self):
-        data = pd.DataFrame(self.inner_data)
-        klass = DataObject.registered_types["pandas_data_frame"]
-        return klass(data, source=self)
