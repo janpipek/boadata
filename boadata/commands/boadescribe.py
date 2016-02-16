b74d3dfa96fd61124965bdd1cf4f453b4eab4a38
Jan Pipek <jan.pipek@gmail.com>
2016-02-16 13:21:40 +0100

    Better argument handling in boadescribe, boatree

--- boadata/commands/boadescribe.py
+++ boadata/commands/boadescribe.py
@@ -4,13 +4,13 @@ import sys
 
 def run_app():
     uri = sys.argv[1]
-    try:
-        do = DataObject.from_uri(uri)
-        print("Type: {0}".format(do.type_name))
-        print("Underlying type: {0}".format(do.inner_data.__class__.__name__))
-        print("Data shape: {0}".format(do.shape))
-        columns = do.columns
-        if columns:
-            print("Column names: {0}".format(", ".join(columns)))
-    except:
-        print("URI not understood.")
+    do = DataObject.from_uri(uri)
+    if not do:
+        print("URI not understood: {0}").format(uri)
+        sys.exit(-1)
+    print("Type: {0}".format(do.type_name))
+    print("Underlying type: {0}".format(do.inner_data.__class__.__name__))
+    print("Data shape: {0}".format(do.shape))
+    columns = do.columns
+    if columns:
+        print("Column names: {0}".format(", ".join(columns)))
--- boadata/commands/boatree.py
+++ boadata/commands/boatree.py
@@ -4,11 +4,12 @@ import sys
 
 def run_app():
     uri = sys.argv[1]
-    try:
-        for cls in DataTree.registered_trees:
-            if cls.accepts_uri(uri):
-                tree = cls(uri)
-    except:
-        print("URI not understood.")
-        exit(-1)
-    tree.dump()
+    tree = None
+    for cls in DataTree.registered_trees:
+        if cls.accepts_uri(uri):
+            tree = cls(uri)
+    if not tree:
+        print("URI not understood: {0}".format(uri))
+        sys.exit(-1)
+    else:
+        tree.dump()
