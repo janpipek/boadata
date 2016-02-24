de8fc5c9a487dcefda2ba8d58d0a887ebaece688
Jan Pipek <jan.pipek@gmail.com>
2016-02-24 16:22:31 +0100

    boacat --limit

--- boadata/commands/boacat.py
+++ boadata/commands/boacat.py
@@ -9,6 +9,7 @@ from boadata import __version__
 @click.argument("uri")
 @click.option("-t", "--type", default=None, help="What type is the object.")
 @click.option("-s", "--sql", required=False, help="SQL to run on the object.")
+@click.option("-l", "--limit", required=False, default=1000, help="Limit the number of rows to be printed.")
 def run_app(uri, type, **kwargs):
     from boadata import load
     do = load(uri, type)
@@ -18,6 +19,9 @@ def run_app(uri, type, **kwargs):
     if "sql" in kwargs:
         do = do.sql(kwargs.get("sql"), table_name="data")
     do = do.convert("pandas_data_frame")
+    if do.shape[0] > kwargs.get("limit", 2*62):
+        print("The row count is higher than the limit ({0}), dropping lines after that...".format(do.shape[0]))
+        do = do.head(kwargs.get("limit"))
 
     from tabulate import tabulate
-    print(tabulate(do.inner_data, do.columns))
+    print(tabulate(do.inner_data, do.columns, tablefmt="orgtbl"))
