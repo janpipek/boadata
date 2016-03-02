be217e9a1ed9f439ba3231c3d94de28cc9d7206d
Jan Pipek <jan.pipek@gmail.com>
2016-03-02 16:54:30 +0100

    shape for XYDataSet

--- boadata/data/plotting_types.py
+++ boadata/data/plotting_types.py
@@ -28,6 +28,17 @@ class XYPlotDataSeriesBase(DataObject):
     def __to_xy_dataseries__(self, **kwargs):
         return XYPlotDataSeries(x=self.x, y=self.y, xname=self.xname, yname=self.yname, source=self)
 
+    def __to_pandas_data_frame__(self, **kwargs):
+        data = pd.DataFrame()
+        data[self.xname] = self.inner_data[0]
+        data[self.yname] = self.inner_data[1]
+        klass = DataObject.registered_types["pandas_data_frame"]
+        return klass(inner_data=data, source=self)
+
+    @property
+    def shape(self):
+        return (len(self.x), 2)
+
 
 @DataObject.register_type()
 class XYPlotDataSeries(XYPlotDataSeriesBase):
