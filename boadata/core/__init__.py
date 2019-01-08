5d8a6305dd3e0e5e4cff756d7c6cf5c02e394c96
Jan Pipek <jan.pipek@showmax.com>
2019-01-08 10:34:08 +0100

    Ignore odo/pandas warning

--- boadata/core/__init__.py
+++ boadata/core/__init__.py
@@ -1,7 +1,12 @@
 '''Classes for data abstraction used in boadata.'''
 
+import warnings
+warnings.filterwarnings("ignore", "pandas.tslib is deprecated")
+
 from .data_properties import DataProperties
 from .data_object import DataObject
 from .data_node import DataNode
 from .data_tree import DataTree
 from .data_conversion import DataConversion
+
+del warnings
