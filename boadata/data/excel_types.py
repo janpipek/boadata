8973588a5b8471b740d2e54b57d04e8d874dac37
Jan Pipek <jan.pipek@showmax.com>
2019-01-15 15:39:44 +0100

    ExcelSheet.accepts_uri

--- boadata/data/excel_types.py
+++ boadata/data/excel_types.py
@@ -1,12 +1,27 @@
-from boadata.core import DataObject
-from .pandas_types import PandasDataFrameBase
+import os
 import pandas as pd
 import xlrd
+
+from boadata.core import DataObject
+from boadata.data.pandas_types import PandasDataFrameBase
 
 
 @DataObject.register_type()
 class ExcelSheet(PandasDataFrameBase):
     type_name = "excel_sheet"
+
+    EXTENSIONS = [".xls", ".xlsx"]
+
+    @classmethod
+    def accepts_uri(cls, uri):
+        if "::" in uri:
+            file, sheet = uri.split("::")
+        else:
+            file, sheet = uri, None
+        file = os.path.abspath(file)
+        dir = os.path.dirname(file)
+        ext = os.path.splitext(os.path.basename(file))[1].lower()
+        return os.path.isdir(dir) and ext in cls.EXTENSIONS
 
     @classmethod
     def from_uri(cls, uri, **kwargs):
