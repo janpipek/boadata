b566a6eb5287f69645f73388091a08a83db92375
Jan Pipek <jan.pipek@showmax.com>
2019-01-15 16:31:32 +0100

    boaconvert without output uri

--- boadata/commands/boaconvert.py
+++ boadata/commands/boaconvert.py
@@ -7,7 +7,7 @@ import click
 @click.command()
 @click.version_option(__version__)
 @click.argument("from_uri")
-@click.argument("to_uri")
+@click.argument("to_uri", required=False, default=None)
 @click.option("-t", "--type", default=None, help="What type should be the destination object.")
 def run_app(from_uri, to_uri, **kwargs):
     kwargs = {key: value for key, value in kwargs.items() if value is not None}
@@ -17,14 +17,21 @@ def run_app(from_uri, to_uri, **kwargs):
     if not do:
         print("URI not understood: {0}").format(from_uri)
         sys.exit(-1)
-    if not type:
-        for conversion in do.allowed_conversions:
-            type_candidate = DataObject.registered_types[conversion[1]]
-            if type_candidate.accepts_uri(to_uri):
-                type = conversion[1]
-                break
-    if not type:
-        print("No suitable output type found.")
-        exit(-1)
 
-    do.convert(type, uri=to_uri)
+    if to_uri:
+        if not type:
+            for conversion in do.allowed_conversions:
+                type_candidate = DataObject.registered_types[conversion[1]]
+                if type_candidate.accepts_uri(to_uri):
+                    type = conversion[1]
+                    break
+        if not type:
+            print("No suitable output type found.")
+            exit(-1)
+
+        do.convert(type, uri=to_uri)
+    else:
+        print("Allowed conversions")
+        print("-------------------")
+        for conversion in do.allowed_conversions:
+            print(conversion[1])
