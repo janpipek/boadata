6f0642bf1146bb2c575d34cd0d750712c6af6e3a
Jan Pipek <jan.pipek@showmax.com>
2019-02-07 20:17:21 +0100

    JSON with structure

--- boadata/data/json_types.py
+++ boadata/data/json_types.py
@@ -1,7 +1,13 @@
-from boadata.core import DataObject, DataConversion
-from boadata.core.data_conversion import IdentityConversion, ChainConversion
-import pandas as pd
+import json
+import os
+from typing import Optional
+
 import odo
+import pandas as pd
+
+from boadata.core import DataConversion, DataObject
+from boadata.core.data_conversion import ChainConversion, IdentityConversion
+
 from .pandas_types import PandasDataFrameBase
 
 
@@ -12,16 +18,27 @@ class JsonFileDataset(PandasDataFrameBase):
     type_name = "json"
 
     @classmethod
-    def accepts_uri(cls, uri):
+    def accepts_uri(cls, uri: str) -> bool:
         return uri and uri.endswith(".json")
 
+    @staticmethod
+    def _read_normalized_lines(uri: str, **kwargs) -> pd.DataFrame:
+        with open(uri, "r") as infile:
+            objects = [json.loads(line) for line in infile if line.strip()]
+        normalized_objects = pd.io.json.json_normalize(objects)
+        return pd.DataFrame(normalized_objects, **kwargs)
+
     @classmethod
-    def from_uri(cls, uri, index_col=False, source=None, **kwargs):
+    def from_uri(cls, uri: str, index_col: bool = False, source: Optional[DataObject] = None, **kwargs) -> "JsonFileDataset":
         resource = odo.resource(uri)
         methods = [
+            lambda: pd.read_json(uri, **kwargs),
+            lambda: JsonFileDataset._read_normalized_lines(uri, **kwargs),
+            lambda: pd.read_json(uri, lines=True, **kwargs),
             lambda: odo.odo(uri, pd.DataFrame, index_col=index_col, **kwargs),
-            lambda: pd.read_json(uri, **kwargs)
         ]
+
+        result = None
         for method in methods:
             try:
                 data = method()
@@ -29,10 +46,9 @@ class JsonFileDataset(PandasDataFrameBase):
                 break
             except:
                 pass
+        
         if result:
             if not result.name:
-                import os
                 result.inner_data.name = os.path.splitext(os.path.basename(uri))[0]
             return result
         raise RuntimeError("No JSON reading method understands the file.")    
-
