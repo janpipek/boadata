0cb799ab7331e21de7cf3b5232d752195b5ec71b
Jan Pipek <jan.pipek@gmail.com>
2020-02-10 17:52:28 +0100

    Print index in boasdescribe

--- boadata/commands/boadescribe.py
+++ boadata/commands/boadescribe.py
@@ -21,6 +21,16 @@ def run_app(uri, type, parameter, **kwargs):
     print("Underlying type: {0}".format(do.inner_data.__class__.__name__))
     print("Data shape: {0}".format(do.shape))
     columns = do.columns
+    if hasattr(do.inner_data, "index"):
+        # TODO: Include in the interface rather...
+        print("Index:")
+        index = do.inner_data.index
+        s = f"  - {index.name or '<no name>'}"
+        try:
+            s += " (dtype={0})".format(index.dtype)
+        except:
+            pass
+        print(s)
     if columns:
         print("Columns:")
         for name in columns:
@@ -30,6 +40,7 @@ def run_app(uri, type, parameter, **kwargs):
             except:
                 pass
             print(s)
+    
 
 
 if __name__ == "__main__":
